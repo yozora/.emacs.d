5f8de0fc35dc8eac3171860dc5536ae7c60b1a8e
Yozora <yozora@gmail.com>
2016-03-10 01:05:07 -0500

    Removing flycheck from purescript

--- yoz/yoz-purescript.el
+++ yoz/yoz-purescript.el
@@ -8,20 +8,4 @@
 
 (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
 
-(eval-after-load 'flycheck
-  '(progn
-     (flycheck-define-checker
-      pulp
-      "Use Pulp to flycheck PureScript code."
-      :command ("pulp" "build" "--monochrome")
-      :error-patterns
-      ((error line-start
-              (or (and "Error at " (file-name)    "line " line ", column " column ":"
-                       (zero-or-more " "))
-                  (and "\"" (file-name) "\" (line " line ", column " column "):"))
-              (message (one-or-more (not (in "*"))))
-              line-end))
-      :modes purescript-mode)
-     (add-to-list 'flycheck-checkers 'pulp)))
-                              
 (provide 'yoz-purescript)
