77ca57413f4d64adf2ece14d1aebc345e805898f
Yozora <yozora@gmail.com>
2016-03-10 01:09:04 -0500

    Revert "Removing flycheck from purescript"
    This reverts commit 5f8de0fc35dc8eac3171860dc5536ae7c60b1a8e.

--- yoz/yoz-purescript.el
+++ yoz/yoz-purescript.el
@@ -8,4 +8,20 @@
 
 (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
 
+(eval-after-load 'flycheck
+  '(progn
+     (flycheck-define-checker
+      pulp
+      "Use Pulp to flycheck PureScript code."
+      :command ("pulp" "build" "--monochrome")
+      :error-patterns
+      ((error line-start
+              (or (and "Error at " (file-name)    "line " line ", column " column ":"
+                       (zero-or-more " "))
+                  (and "\"" (file-name) "\" (line " line ", column " column "):"))
+              (message (one-or-more (not (in "*"))))
+              line-end))
+      :modes purescript-mode)
+     (add-to-list 'flycheck-checkers 'pulp)))
+                              
 (provide 'yoz-purescript)
