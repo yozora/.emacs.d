bc7302a01c8a582b447a12633f2fc92b28ab25e3
Yozora <yozora@gmail.com>
2016-03-14 13:53:55 -0400

    Replaced custom purescript flychecker with flycheck-purescript

--- yoz/yoz-purescript.el
+++ yoz/yoz-purescript.el
@@ -8,20 +8,9 @@
 
 (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
 
+(package-require 'flycheck-purescript)
+
 (eval-after-load 'flycheck
-  '(progn
-     (flycheck-define-checker
-      pulp
-      "Use Pulp to flycheck PureScript code."
-      :command ("pulp" "build" "--monochrome")
-      :error-patterns
-      ((error line-start
-              (or (and "Error at " (file-name)    "line " line ", column " column ":"
-                       (zero-or-more " "))
-                  (and "\"" (file-name) "\" (line " line ", column " column "):"))
-              (message (one-or-more (not (in "*"))))
-              line-end))
-      :modes purescript-mode)
-     (add-to-list 'flycheck-checkers 'pulp)))
+      '(flycheck-purescript-setup))
                               
 (provide 'yoz-purescript)
